aa047b59e5cfdb0a25c77d4ed58c254b3bf44dc7
Jake Dale <2665658d@student.gla.ac.uk>
2023-03-10 10:35:39 +0000

    Commit attempt

--- quizapp/migrations/0001_initial.py
+++ quizapp/migrations/0001_initial.py
@@ -9,7 +9,6 @@ import django.utils.timezone
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -23,17 +22,33 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')),
+                ('is_superuser', models.BooleanField(default=False,
+                                                     help_text='Designates that this user has all permissions without explicitly assigning them.',
+                                                     verbose_name='superuser status')),
+                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'},
+                                              help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+                                              max_length=150, unique=True,
+                                              validators=[django.contrib.auth.validators.UnicodeUsernameValidator()],
+                                              verbose_name='username')),
                 ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
                 ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
                 ('email', models.EmailField(blank=True, max_length=254, verbose_name='email address')),
-                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('is_staff', models.BooleanField(default=False,
+                                                 help_text='Designates whether the user can log into this admin site.',
+                                                 verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True,
+                                                  help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
+                                                  verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('user_type', models.PositiveSmallIntegerField(choices=[(1, 'quiz taker'), (2, 'quiz maker')], default=1)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('user_type',
+                 models.PositiveSmallIntegerField(choices=[(1, 'quiz taker'), (2, 'quiz maker')], default=1)),
+                ('groups', models.ManyToManyField(blank=True,
+                                                  help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                                                  related_name='user_set', related_query_name='user', to='auth.group',
+                                                  verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                                                            related_name='user_set', related_query_name='user',
+                                                            to='auth.permission', verbose_name='user permissions')),
             ],
             options={
                 'verbose_name': 'user',
@@ -60,14 +75,16 @@ class Migration(migrations.Migration):
             name='QuizTaker',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                'user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='QuizMaker',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                'user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -77,8 +94,11 @@ class Migration(migrations.Migration):
                 ('max_score', models.IntegerField()),
                 ('actual_score', models.IntegerField()),
                 ('quiz_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='quizapp.quiz')),
-                ('quiz_maker', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='quiz_author', to=settings.AUTH_USER_MODEL)),
-                ('quiz_taker', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('quiz_maker',
+                 models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='quiz_author',
+                                   to=settings.AUTH_USER_MODEL)),
+                ('quiz_taker',
+                 models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -87,24 +107,8 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('max_score', models.IntegerField()),
                 ('actual_score', models.IntegerField()),
-                ('quiz_instance_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='quizapp.quizinstance')),
-<<<<<<< HEAD
-=======
-            ],
-        ),
-        migrations.CreateModel(
-            name='Question',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('question_text', models.TextField(max_length=5000)),
-                ('image', models.ImageField(upload_to='')),
-                ('max_score', models.PositiveIntegerField()),
-                ('correct_answer', models.TextField(max_length=200)),
-                ('incorrect_answer_1', models.TextField(max_length=200)),
-                ('incorrect_answer_2', models.TextField(max_length=200)),
-                ('incorrect_answer_3', models.TextField(max_length=200)),
-                ('quiz', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='quizapp.quiz')),
->>>>>>> a945e36bb63d501d1679ab705439a2a069c52e4f
+                ('quiz_instance_id',
+                 models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='quizapp.quizinstance')),
             ],
         ),
         migrations.CreateModel(

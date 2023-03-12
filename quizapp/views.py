5b7ba5839312282bce3e313af8277eb201ccf403
Jake Dale <2665658d@student.gla.ac.uk>
2023-03-12 17:45:31 +0000

    Commit so I can pull changes

--- quizapp/views.py
+++ quizapp/views.py
@@ -26,6 +26,7 @@ def index(request):
     else:
         return render(request, 'quizapp/index.html', {'username': request.user.username})
 
+
 def register(request):
     registered = False
 
@@ -66,9 +67,11 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('quizapp:index'))
 
+
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.forms import formset_factory
 from .models import Quiz, Question
+
 
 @quiz_taker_required
 def quiz(request, quiz_slug):
@@ -80,7 +83,8 @@ def quiz(request, quiz_slug):
     question_options = []
     for question in questions:
         # add options to question_options list
-        options = [question.correct_answer, question.incorrect_answer_1, question.incorrect_answer_2, question.incorrect_answer_3]
+        options = [question.correct_answer, question.incorrect_answer_1, question.incorrect_answer_2,
+                   question.incorrect_answer_3]
         question_options.append([question, options])
 
     context = {
@@ -92,61 +96,68 @@ def quiz(request, quiz_slug):
     }
     return render(request, 'quizapp/quiz.html', context)
 
+
 def about(request):
     return render(request, 'quizapp/about.html')
+
 
 def contact(request):
     return render(request, 'quizapp/contact.html')
 
+
 @quiz_maker_required
 def quiz_maker_dashboard(request):
-    has_quizzes=False
+    has_quizzes = False
     if Quiz.objects.filter(author=request.user).exists():
-        has_quizzes=True
+        has_quizzes = True
     my_quizzes = Quiz.objects.filter(author=request.user)
-      
-    context={'my_quizzes':my_quizzes,'has_quizzes':has_quizzes}
+
+    context = {'my_quizzes': my_quizzes, 'has_quizzes': has_quizzes}
     return render(request, 'quizapp/quizmaker.html', context)
+
 
 @quiz_taker_required
 def quiz_taker_dashboard(request):
-    quizzes_exist=False
+    quizzes_exist = False
     if Quiz.objects.all().exists():
-        quizzes_exist=True
+        quizzes_exist = True
     all_quizzes = Quiz.objects.all()
-      
-    context={'all_quizzes':all_quizzes,'quizzes_exist':quizzes_exist}
+
+    context = {'all_quizzes': all_quizzes, 'quizzes_exist': quizzes_exist}
     return render(request, 'quizapp/quiztaker.html', context)
+
 
 @quiz_maker_required
 def create_quiz(request):
     if request.method == 'POST':
-        form=QuizForm(request.POST)
+        form = QuizForm(request.POST)
         if form.is_valid():
-            form.instance.max_score=form.instance.number_of_questions
+            form.instance.max_score = form.instance.number_of_questions
             form.instance.author = request.user
             form.instance.author_id = request.user.id
             form.instance.name_slug = slugify(form.instance.name)
             form.save()
-            return redirect(reverse('quizapp:add_questions',kwargs={'quiz_slug': form.instance.name_slug,'number_of_questions':form.instance.number_of_questions}))
-    context={ "form" : QuizForm, "username" : request.user.username }
-    
+            return redirect(reverse('quizapp:add_questions', kwargs={'quiz_slug': form.instance.name_slug,
+                                                                     'number_of_questions': form.instance.number_of_questions}))
+    context = {"form": QuizForm, "username": request.user.username}
+
     return render(request, 'quizapp/create_quiz.html', context)
 
+
 @quiz_maker_required
-def add_questions(request,quiz_slug, number_of_questions):
-    MyFormSet = formset_factory(QuestionForm, extra=number_of_questions)
+def add_questions(request, quiz_slug, number_of_questions):
+    my_form_set = formset_factory(QuestionForm, extra=number_of_questions)
     if request.method == 'POST':
-        formset = MyFormSet(request.POST)
+        formset = my_form_set(request.POST)
         if formset.is_valid():
-            overall_max_score=0
+            overall_max_score = 0
             for form in formset:
-                form.instance.quiz=Quiz.objects.get(name_slug=quiz_slug)
-                form.instance.quiz_id=Quiz.objects.get(name_slug=quiz_slug).id
+                form.instance.quiz = Quiz.objects.get(name_slug=quiz_slug)
+                form.instance.quiz_id = Quiz.objects.get(name_slug=quiz_slug).id
                 form.save()
-                overall_max_score+=form.instance.max_score
-            Quiz.objects.update_or_create(name_slug = quiz_slug, defaults={'max_score':overall_max_score})
+                overall_max_score += form.instance.max_score
+            Quiz.objects.update_or_create(name_slug=quiz_slug, defaults={'max_score': overall_max_score})
             return redirect(reverse('quizapp:quizmaker'))
     else:
-        formset = MyFormSet()
-    return render(request, 'quizapp/add_questions.html', {'formset': formset, "username" : request.user.username })
+        formset = my_form_set()
+    return render(request, 'quizapp/add_questions.html', {'formset': formset, "username": request.user.username})
